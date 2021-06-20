4d078b15d89f29a24dd318b24c1fadb6dfc0bd24
Abdulla Masood <Oakwood@Abdullas-MacBook-Pro.local>
2021-06-20 22:47:23 +0500

    window size and some assets added

--- main.lua
+++ main.lua
@@ -1,13 +1,27 @@
 -- main game template file
 function love.load()
+    love.graphics.print("Hello World", 400,300)
+    x = 100
+    y = 200
 
+    --Window width and window height
+    pWidth = love.graphics.getPixelWidth()
+    pHeight = love.graphics.getPixelHeight()
 end
 
 function love.update()
- 
+  whale = love.graphics.newImage("whale.png")
+  x = x + 0.5
+  y  = y + 1.5
+  
 end
 
 function love.draw()
-  love.graphics.rectangle("line", 100, 50, 200,150)
+ -- love.graphics.rectangle("line", 100, 50, 200,150)
+    -- love.graphics.draw(whale,x,y,0.3,0.3)
+    -- love.graphics.setBackgroundColor(0,0,0.5)
+    love.graphics.print(pWidth, 400,300)
+    love.graphics.print(pHeight,300,200)
+
 end
 
