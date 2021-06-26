710bca6af4dbe3b8665a9e5658c222e8fc145952
Abdulla Masood <Oakwood@Abdullas-MacBook-Pro.local>
2021-06-26 16:29:49 +0500

    some updates

--- circle.lua
+++ circle.lua
@@ -1,7 +1,7 @@
 Circle = Shape:extend()
 
 function Circle:new(x,y,radius)
-    Circle.super.new(self,x,y)
+    Circle.super.new(self,x,y)  --these are the features extending from Shape file   the super method means its using the shape classes values
     self.radius = radius
 end
 
