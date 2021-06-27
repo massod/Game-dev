d7eb4815740599ff1ef79334286420b1c161928d
Abdulla Masood <Oakwood@Abdullas-MacBook-Pro.local>
2021-06-27 18:57:37 +0500

    collidion detection added

--- main.lua
+++ main.lua
@@ -1,28 +1,48 @@
 
 function love.load()
-Object =  require "classic" --this file imitates oop behaviour accross the program
-require "shape"
-require "circle"    --refrences to indivdual files
-require "rectangle"
-obj1 = Circle(100,100,75)
-obj2 = Circle(300,300,85)
-obj3 = Rectangle(500,450,80,30)
+  require "collision"
+  
+  r1 = {
+    x = 10,
+    y = 100,
+    width = 100,
+    height = 100
+  }
+  --creating two rectangles based on table data
+  r2 = {
+    x = 250,
+    y = 120,
+    width = 150,
+    height = 120
+  }
+  
 end
 
 function love.update(dt)
-    obj1:update(dt)
-    obj2:update(dt)
-    obj3:update(dt)
-    --reverse function
-    obj1:reverse()
-    obj2:reverse()
-    obj3:reverse()
+-- make one rectangle move in this case r1
+   posX = love.mouse.getX()
+   posY = love.mouse.getY()
+   --r1.x = r1.x + 100 * dt
+   r1.x = posX
+   r1.y = posY
     
+
 end
 
 function love.draw()
-   obj1:draw()
-   obj2:draw()
-   obj3:draw()
+
+  local mode
+  if collision_detection(r1,r2) then
+    mode = "fill"
+  else
+    mode = "line"
+  end
+  love.graphics.setColor(1,0,0)
+  love.graphics.rectangle(mode,r1.x,r1.y,r1.width,r1.height)
+  love.graphics.setColor(0,0,1)
+  love.graphics.rectangle(mode, r2.x,r2.y,r2.width,r2.height)
+  love.graphics.setColor(0,1,0)
+  love.graphics.print(posX ,400,300)
+  love.graphics.print(posY,450,300)
 end
 
